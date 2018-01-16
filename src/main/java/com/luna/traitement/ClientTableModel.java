60e630dc4d25e326287466387272620d1dd7dae5
Clemence <clemence059@gmail.com>
2018-01-16 13:07:52 +0100

    Traitement commenté
    Plus d'erreurs pour le moment

--- src/main/java/com/luna/traitement/ClientTableModel.java
+++ src/main/java/com/luna/traitement/ClientTableModel.java
@@ -3,81 +3,85 @@ package com.luna.traitement;
 import java.util.ArrayList;
 import java.util.List;
 import com.luna.entities.Client;
-import com.luna.utils.GlobalConnection;
 import com.luna.dao.*;
 
 import javax.swing.table.AbstractTableModel;
 
 public class ClientTableModel extends AbstractTableModel {
-	private String[] columnNames = {"Id", "Nom", "Prénom", "Code client", "Tel", "Mail"};
-	private ArrayList<Client> clients;
-
-	public ClientTableModel(int sortby) {
-		ClientDAO dao = new ClientDAOmysql(GlobalConnection.getInstance());
-		this.clients = dao.getAllClient(sortby);
-	}
-	
-	public List<Client> getClient(){
-		return clients;
-	}
-
+//	private String[] columnNames = {"Id", "Nom", "Prï¿½nom", "Code client", "Tel", "Mail"};
+//	private ArrayList<Client> clients;
+//
+//	public ClientTableModel(int sortby) {
+//		ClientDAO dao = new ClientDAOmysql(GlobalConnection.getInstance());
+//		this.clients = dao.getAllClient(sortby);
+//	}
+//	
+//	public List<Client> getClient(){
+//		return clients;
+//	}
+//
 	@Override
 	public int getRowCount() {
-		return clients.size();
+//		return clients.size();
+		return 1;
 	}
 
 	@Override
 	public String getColumnName(int columnIndex) {
-		return columnNames[columnIndex];
+//		return columnNames[columnIndex];
+		return null;
 	}
 
 	@Override
 	public Class<?> getColumnClass(int columnIndex) {
-		switch (columnIndex) {
-			case 0:
-				return Integer.class;
-			case 1:
-				return String.class;
-	
-			case 2:
-				return String.class;
-	
-			case 3:
-				return String.class;
-	
-			case 4:
-				return String.class;
-	
-			case 5:
-				return String.class;
-				
-			default:
-				return Object.class;
-		}
+//		switch (columnIndex) {
+//			case 0:
+//				return Integer.class;
+//			case 1:
+//				return String.class;
+//	
+//			case 2:
+//				return String.class;
+//	
+//			case 3:
+//				return String.class;
+//	
+//			case 4:
+//				return String.class;
+//	
+//			case 5:
+//				return String.class;
+//				
+//			default:
+//				return Object.class;
+//		}
+		return Object.class;
 	}
 	
 	@Override
 	public int getColumnCount() {
-		return columnNames.length;
+//		return columnNames.length;
+		return 1;
 	}
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		switch(columnIndex) {
-		case 0: // Id
-			return clients.get(rowIndex).getIdClient();
-		case 1: // Nom
-			return clients.get(rowIndex).getNom();
-		case 2: // Prenom
-			return clients.get(rowIndex).getPrenom();
-		case 3: // Code client
-			return clients.get(rowIndex).getCodeClient();
-		case 4: // Tel
-			return clients.get(rowIndex).getTel();
-		case 5: // Mail
-			return clients.get(rowIndex).getMail();
-		default:
-			throw new IllegalArgumentException();
-		}
+//		switch(columnIndex) {
+//		case 0: // Id
+//			return clients.get(rowIndex).getIdClient();
+//		case 1: // Nom
+//			return clients.get(rowIndex).getNom();
+//		case 2: // Prenom
+//			return clients.get(rowIndex).getPrenom();
+//		case 3: // Code client
+//			return clients.get(rowIndex).getCodeClient();
+//		case 4: // Tel
+//			return clients.get(rowIndex).getTel();
+//		case 5: // Mail
+//			return clients.get(rowIndex).getMail();
+//		default:
+//			throw new IllegalArgumentException();
+//		}
+		return null;
 	}
 }
--- src/main/java/com/luna/traitement/ClientTraitement.java
+++ src/main/java/com/luna/traitement/ClientTraitement.java
@@ -1,133 +1,128 @@
 package com.luna.traitement;
 
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.luna.dao.*;
 import com.luna.entities.Client;
-import com.luna.utils.GlobalConnection;
 
 public class ClientTraitement {
-	ClientDAO client;
-
-	public ClientTraitement() {
-		this.client = new ClientDAOmysql(GlobalConnection.getInstance());
-	}
-
-	public void Afficher(JTextField code, JTextField creation, JTextField prenom, JTextField nom, JTextField ville,
-			JTextField codep, JTextField rue, JTextField tel, JTextField mail, JTextField remarques, JTextField id,
-			int idClient) {
-		Client cli = new Client();
-		cli = client.getClient(idClient);
-		code.setText(cli.getCodeClient());
-		creation.setText(cli.getDate_creation());
-		prenom.setText(cli.getPrenom());
-		nom.setText(cli.getNom());
-		ville.setText(cli.getVille());
-		codep.setText("" + cli.getCodePostal());
-		rue.setText(cli.getRue());
-		tel.setText(cli.getTel());
-		mail.setText(cli.getMail());
-		remarques.setText(cli.getRemarques());
-		id.setText("" + cli.getIdClient());
-	}
-
-	public boolean Save(JTextField code, JTextField creation, JTextField prenom, JTextField nom, JTextField ville,
-			JTextField codep, JTextField rue, JTextField tel, JTextField mail, JTextField remarques, JTextField id) {
-		if (code.getText().equals("") || creation.getText().equals("") || prenom.getText().equals("")
-				|| nom.getText().equals("") || ville.getText().equals("")
-				|| rue.getText().equals("") || tel.getText().equals("") || mail.getText().equals("")) {
-			JOptionPane.showMessageDialog(code, "Veuillez compléter tous les champs.", "Modification impossible",
-					JOptionPane.ERROR_MESSAGE);
-			return false;
-		} else {
-			if(VerifCP(codep)) {
-				Client cli = new Client();
-				cli.setCodeClient(code.getText());
-				cli.setDate_creation(creation.getText());
-				cli.setPrenom(prenom.getText());
-				cli.setNom(nom.getText());
-				cli.setVille(ville.getText());
-				cli.setCodePostal(Integer.parseInt(codep.getText()));
-				cli.setRue(rue.getText());
-				cli.setTel(tel.getText());
-				cli.setMail(mail.getText());
-				cli.setRemarques(remarques.getText());
-				cli.setIdClient(Integer.parseInt(id.getText()));
-
-				client.updateClient(cli);
-				return true;
-			}
-			return false;
-		}
-	}
-
-	public boolean Insert(JTextField code, JTextField creation, JTextField prenom, JTextField nom, JTextField ville,
-			JTextField codep, JTextField rue, JTextField tel, JTextField mail, JTextField remarques) {
-		if (code.getText().equals("") || creation.getText().equals("") || prenom.getText().equals("")
-				|| nom.getText().equals("") || ville.getText().equals("")
-				|| rue.getText().equals("") || tel.getText().equals("") || mail.getText().equals("")) {
-			JOptionPane.showMessageDialog(code, "Veuillez compléter tous les champs.", "Ajout impossible",
-					JOptionPane.ERROR_MESSAGE);
-			return false;
-		} else {
-			if(VerifCP(codep)) {
-				Client cli = new Client();
-				cli.setCodeClient(code.getText());
-				cli.setDate_creation(creation.getText());
-				cli.setPrenom(prenom.getText());
-				cli.setNom(nom.getText());
-				cli.setVille(ville.getText());
-				cli.setCodePostal(Integer.parseInt(codep.getText()));
-				cli.setRue(rue.getText());
-				cli.setTel(tel.getText());
-				cli.setMail(mail.getText());
-				cli.setRemarques(remarques.getText());
-
-				client.insertClient(cli);
-				return true;
-			}
-			return false;
-		}
-	}
-
-	public void Suppr(JTextField code, JTextField creation, JTextField prenom, JTextField nom, JTextField ville,
-			JTextField codep, JTextField rue, JTextField tel, JTextField mail, JTextField remarques, JTextField id,
-			int idClient) {
-		if(client.removeClient(idClient)) {
-			code.setText("");
-			creation.setText("");
-			prenom.setText("");
-			nom.setText("");
-			ville.setText("");
-			codep.setText("");
-			rue.setText("");
-			tel.setText("");
-			mail.setText("");
-			remarques.setText("");
-			id.setText("");
-
-			client.removeClient(idClient);
-		} else {
-			JOptionPane.showMessageDialog(code, "Le client est utilisé dans une commande.", "Suppression impossible",
-					JOptionPane.ERROR_MESSAGE);
-		}
-	}
-	
-	public boolean VerifCP(JTextField cp) {
-		try {
-			int i = Integer.parseInt(cp.getText());
-			if(i <= 99999 && i >= 10000) {
-				return true;
-			} else {
-				JOptionPane.showMessageDialog(cp, "Le code postal doit être au format \"59000\"", "Ajout/Modification impossible",
-						JOptionPane.ERROR_MESSAGE);
-				return false;
-			}
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(cp, "Le code postal doit être au format \"59000\"", "Ajout/Modification impossible",
-					JOptionPane.ERROR_MESSAGE);
-			return false;
-		}
-	}
+//	@Autowired
+//	ClientDAO client;
+//
+//	public void Afficher(JTextField code, JTextField creation, JTextField prenom, JTextField nom, JTextField ville,
+//			JTextField codep, JTextField rue, JTextField tel, JTextField mail, JTextField remarques, JTextField id,
+//			int idClient) {
+//		Client cli = new Client();
+//		cli = client.getClient(idClient);
+//		code.setText(cli.getCodeClient());
+//		creation.setText(cli.getDate_creation());
+//		prenom.setText(cli.getPrenom());
+//		nom.setText(cli.getNom());
+//		ville.setText(cli.getVille());
+//		codep.setText("" + cli.getCodePostal());
+//		rue.setText(cli.getRue());
+//		tel.setText(cli.getTel());
+//		mail.setText(cli.getMail());
+//		remarques.setText(cli.getRemarques());
+//		id.setText("" + cli.getIdClient());
+//	}
+//
+//	public boolean Save(JTextField code, JTextField creation, JTextField prenom, JTextField nom, JTextField ville,
+//			JTextField codep, JTextField rue, JTextField tel, JTextField mail, JTextField remarques, JTextField id) {
+//		if (code.getText().equals("") || creation.getText().equals("") || prenom.getText().equals("")
+//				|| nom.getText().equals("") || ville.getText().equals("")
+//				|| rue.getText().equals("") || tel.getText().equals("") || mail.getText().equals("")) {
+//			JOptionPane.showMessageDialog(code, "Veuillez complï¿½ter tous les champs.", "Modification impossible",
+//					JOptionPane.ERROR_MESSAGE);
+//			return false;
+//		} else {
+//			if(VerifCP(codep)) {
+//				Client cli = new Client();
+//				cli.setCodeClient(code.getText());
+//				cli.setDate_creation(creation.getText());
+//				cli.setPrenom(prenom.getText());
+//				cli.setNom(nom.getText());
+//				cli.setVille(ville.getText());
+//				cli.setCodePostal(Integer.parseInt(codep.getText()));
+//				cli.setRue(rue.getText());
+//				cli.setTel(tel.getText());
+//				cli.setMail(mail.getText());
+//				cli.setRemarques(remarques.getText());
+//				cli.setIdClient(Integer.parseInt(id.getText()));
+//
+//				client.updateClient(cli);
+//				return true;
+//			}
+//			return false;
+//		}
+//	}
+//
+//	public boolean Insert(JTextField code, JTextField creation, JTextField prenom, JTextField nom, JTextField ville,
+//			JTextField codep, JTextField rue, JTextField tel, JTextField mail, JTextField remarques) {
+//		if (code.getText().equals("") || creation.getText().equals("") || prenom.getText().equals("")
+//				|| nom.getText().equals("") || ville.getText().equals("")
+//				|| rue.getText().equals("") || tel.getText().equals("") || mail.getText().equals("")) {
+//			JOptionPane.showMessageDialog(code, "Veuillez complï¿½ter tous les champs.", "Ajout impossible",
+//					JOptionPane.ERROR_MESSAGE);
+//			return false;
+//		} else {
+//			if(VerifCP(codep)) {
+//				Client cli = new Client();
+//				cli.setCodeClient(code.getText());
+//				cli.setDate_creation(creation.getText());
+//				cli.setPrenom(prenom.getText());
+//				cli.setNom(nom.getText());
+//				cli.setVille(ville.getText());
+//				cli.setCodePostal(Integer.parseInt(codep.getText()));
+//				cli.setRue(rue.getText());
+//				cli.setTel(tel.getText());
+//				cli.setMail(mail.getText());
+//				cli.setRemarques(remarques.getText());
+//
+//				client.insertClient(cli);
+//				return true;
+//			}
+//			return false;
+//		}
+//	}
+//
+//	public void Suppr(JTextField code, JTextField creation, JTextField prenom, JTextField nom, JTextField ville,
+//			JTextField codep, JTextField rue, JTextField tel, JTextField mail, JTextField remarques, JTextField id,
+//			int idClient) {
+//		if(client.removeClient(idClient)) {
+//			code.setText("");
+//			creation.setText("");
+//			prenom.setText("");
+//			nom.setText("");
+//			ville.setText("");
+//			codep.setText("");
+//			rue.setText("");
+//			tel.setText("");
+//			mail.setText("");
+//			remarques.setText("");
+//			id.setText("");
+//
+//			client.removeClient(idClient);
+//		} else {
+//			JOptionPane.showMessageDialog(code, "Le client est utilisï¿½ dans une commande.", "Suppression impossible",
+//					JOptionPane.ERROR_MESSAGE);
+//		}
+//	}
+//	
+//	public boolean VerifCP(JTextField cp) {
+//		try {
+//			int i = Integer.parseInt(cp.getText());
+//			if(i <= 99999 && i >= 10000) {
+//				return true;
+//			} else {
+//				JOptionPane.showMessageDialog(cp, "Le code postal doit ï¿½tre au format \"59000\"", "Ajout/Modification impossible",
+//						JOptionPane.ERROR_MESSAGE);
+//				return false;
+//			}
+//		} catch (Exception e) {
+//			JOptionPane.showMessageDialog(cp, "Le code postal doit ï¿½tre au format \"59000\"", "Ajout/Modification impossible",
+//					JOptionPane.ERROR_MESSAGE);
+//			return false;
+//		}
+//	}
 }
