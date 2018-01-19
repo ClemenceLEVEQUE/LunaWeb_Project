19842e9eeeb5775dad10aaec3faded47211b54be
Karim <elkoulalikarim@gmail.com>
2018-01-19 16:11:54 +0100

    login action début

--- src/main/java/com/formation/action/LoginAction.java
+++ src/main/java/com/formation/action/LoginAction.java
@@ -3,22 +3,38 @@ package com.formation.action;
 import java.util.List;
 
 import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Actions;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.luna.entities.User;
 import com.luna.service.UserService;
+import com.luna.traitement.UsersTraitement;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
-
+@Action("login")
+@Results(value= {@Result(name="SUCCESS", location="/index.jsp"),@Result(name="ERROR", location="/login.jsp")})
 public class LoginAction extends ActionSupport implements ModelDriven<User>{
 	
 	private static final long serialVersionUID = 1L;
 	
 	private User user;
-	
 	@Autowired
-	public UserService userService;
+	private UsersTraitement usersTraitement;
 	
+	
+	@Override
+	public String execute() throws Exception {
+		return usersTraitement.Authentification(user);
+	}
+
+
+	@Override
+	public User getModel() {
+		return user;
+	}
+
 
 	public User getUser() {
 		return user;
@@ -30,28 +46,16 @@ public class LoginAction extends ActionSupport implements ModelDriven<User>{
 	}
 
 
-	public UserService getUserService() {
-		return userService;
+	public UsersTraitement getUsersTraitement() {
+		return usersTraitement;
 	}
 
 
-	public void setUserService(UserService userService) {
-		this.userService = userService;
+	public void setUsersTraitement(UsersTraitement usersTraitement) {
+		this.usersTraitement = usersTraitement;
 	}
 
-	@Action("index") 
-public String accueil() {
-	return SUCCESS;
-}
-	
-	
-	
-	@Override
-	public User getModel() {
-		
-		return user;
-	}
-	
+
 
 
 	
