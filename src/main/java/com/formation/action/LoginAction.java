1f5d62c0844548ded77a84716557a4ee36ec1283
Karim <elkoulalikarim@gmail.com>
2018-01-19 14:55:39 +0100

    Login action pour gerer le probleme de merge

--- src/main/java/com/formation/action/LoginAction.java
+++ src/main/java/com/formation/action/LoginAction.java
@@ -1,18 +1,17 @@
 package com.formation.action;
 
+import java.util.List;
+
 import org.apache.struts2.convention.annotation.Action;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.luna.entities.User;
 import com.luna.service.UserService;
-import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 
-@Action("/login")
-@Results({ @Result(name="success", location= "/index.jsp"), @Result(name="error", location="/login.jsp")})
 public class LoginAction extends ActionSupport implements ModelDriven<User>{
-
+	
 	private static final long serialVersionUID = 1L;
 	
 	private User user;
@@ -45,16 +44,16 @@ public String accueil() {
 	return SUCCESS;
 }
 	
-	public String execute() throws Exception{
-		List<User> users =userService.getAllUser();
 	
-	}
 	
 	@Override
 	public User getModel() {
 		
 		return user;
 	}
+	
+
+
 	
 
 }
