11a9761e6c78b4a68b5d360c8f6c45b7d8d5515e
Clemence <clemence059@gmail.com>
2018-01-23 15:47:23 +0100

    ArticleAction

--- src/main/java/com/luna/action/ArticleAction.java
+++ src/main/java/com/luna/action/ArticleAction.java
@@ -2,8 +2,6 @@ package com.luna.action;
 
 import java.util.List;
 
-
-
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
@@ -16,59 +14,43 @@ import com.luna.service.ArticleService;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 
-
-
-
-@Results({ @Result(name = "SUCCESS", location = "listeArticle.jsp"),
-@Result(name= "insert" , location = "listeArticle.jsp"),
-@Result(name="errorAdd", location = "ajoutArticle.jsp"),
-@Result(name="delete" , location= "listeArticle.jsp"),
-@Result(name="errorDelete" , location="listeArticle.jsp"),
-@Result(name="update", location="modifArticle.jsp")
-
-
+@Results({ @Result(name = "SUCCESS", location = "/listeArticle.jsp"),
+		@Result(name = "insert", location = "/listeArticle.jsp"),
+		@Result(name = "errorAdd", location = "/ajoutArticle.jsp"),
+		@Result(name = "delete", location = "/listeArticle.jsp"),
+		@Result(name = "errorDelete", location = "/listeArticle.jsp"),
+		@Result(name = "update", location = "/modifArticle.jsp")
 })
-public class ArticleAction extends ActionSupport implements ModelDriven<Article>{
-
-	/**
-	 * 
-	 */
+public class ArticleAction extends ActionSupport implements ModelDriven<Article> {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Article article;
 	@Autowired
 	private ArticleService articleService;
-    private List<Article> models;
+	private List<Article> models;
 
-	
-@Action("AffichageArticle")
+	@Action("AffichageArticle")
 	@Override
 	public String execute() throws Exception {
-		 setModels(articleService.listArticle());
-		return SUCCESS;
+		setModels();
+		return "SUCCESS";
 	}
-	
-	
+
 	@Action("insertArt")
-	public String insertArticle() throws Exception{
-	return articleService.add(article);
+	public String insertArticle() throws Exception {
+		return articleService.add(article);
 	}
-	
+
 	@Action("deleteArt")
-	public String deleteArticle() throws Exception{
+	public String deleteArticle() throws Exception {
 		return articleService.delete(article.getIdArticle());
 	}
-	
+
 	@Action("updateArt")
-	public String updateArticle() throws Exception{
+	public String updateArticle() throws Exception {
 		return articleService.update(article);
 	}
-	
 
-	
-	
-	
-	
 	public Article getArticle() {
 		return article;
 	}
@@ -77,41 +59,21 @@ public class ArticleAction extends ActionSupport implements ModelDriven<Article>
 		this.article = article;
 	}
 
-
-
-
-
-
-
-
-
 	public ArticleService getArticleService() {
 		return articleService;
 	}
-
-
-
 
 	public void setArticleService(ArticleService articleService) {
 		this.articleService = articleService;
 	}
 
-
-
-
-
-
-
-
 	public List<Article> getModels() {
 		return models;
 	}
 
-
-	public void setModels(List<Article> models) {
-		this.models = models;
+	public void setModels() {
+		this.models = articleService.listArticle();
 	}
-
 
 	@Override
 	public Article getModel() {
