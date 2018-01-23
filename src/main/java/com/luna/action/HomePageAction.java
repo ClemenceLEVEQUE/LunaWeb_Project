fac993455b9b5982b2a9869fc4e4a5989e5cf3b8
Clemence <clemence059@gmail.com>
2018-01-23 10:20:54 +0100

    Erreur résolue, Action HomePage créée, début de la gestion des actions et jsp

---
+++ src/main/java/com/luna/action/HomePageAction.java
@@ -0,0 +1,13 @@
+package com.luna.action;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@Action("index")
+@Result(location = "/login.jsp")
+public class HomePageAction extends ActionSupport {
+	private static final long serialVersionUID = 1L;
+
+}
